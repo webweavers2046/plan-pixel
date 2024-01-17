8ae8aa3e650b226c52daf230bc65ffd17db67fc0
SabbirSami <smd71430@gmail.com>
2024-01-17 11:58:53 +0600

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        Welcome to plan pixel
-      </div>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <h2 className="text-2xl">Welcome to plan pixel</h2>
+            </div>
+        </main>
+    );
 }
