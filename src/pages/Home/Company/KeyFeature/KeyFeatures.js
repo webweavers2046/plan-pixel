5a22aa946369b5c1ee17641e3e87c467713c69eb
Shakil-Ahmmed <Shakilahmmed8882@gmail.com>
2024-01-18 16:12:47 +0600

    [feat]keySection:added title

--- src/pages/Home/Company/KeyFeature/KeyFeatures.js
+++ src/pages/Home/Company/KeyFeature/KeyFeatures.js
@@ -3,7 +3,7 @@ import SectionTitle from "@/components/Common/sectionTitle/SectionTitle";
 const KeyFeatures = () => {
   return (
     <div>
-      <SectionTitle title={"Our Key Features"} />
+      <SectionTitle title={"Our Key Features."} />
     </div>
   );
 };
