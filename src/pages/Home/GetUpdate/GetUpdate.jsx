1485f65f5049b1300758496db4c22b3dfcceef42
SabbirSami <smd71430@gmail.com>
2024-01-19 20:09:30 +0600

    fix get update section

--- src/pages/Home/GetUpdate/GetUpdate.jsx
+++ src/pages/Home/GetUpdate/GetUpdate.jsx
@@ -12,16 +12,16 @@ const GetUpdate = () => {
                 />
                 <form className="md:flex items-center justify-center">
                     <input
-                        className="border-l-2  border-b-2  border-t-2  py-3 px-3 max-w-72 placeholder:font-semibold focus:outline-green-400 "
+                        className="border-y-2  md:border-s-2  md:border-x-0 border-x-2  py-3 px-3 md:max-w-72 w-full placeholder:font-semibold focus:outline-green-400 "
                         placeholder="Your Name"
                         type="text"
                     />
                     <input
-                        className="border-l-2 border-b-2 border-t-2  py-3 px-3 lg:w-96 placeholder:font-semibold focus:outline-green-400"
+                        className="border-y-2  md:border-s-2  md:border-x-0 border-x-2  py-3 px-3 md:w-96 w-full placeholder:font-semibold focus:outline-green-400"
                         placeholder="Your Email"
                         type="email"
                     />
-                    <button className="px-10 py-3 bg-gradient-to-r from-[#50B577] to-[#93C648] text-xl text-white text-bold rounded-r hover:bg-green-400">
+                    <button className="px-10 py-3 md:w-auto w-full bg-gradient-to-r from-[#50B577] to-[#93C648] text-xl text-white text-bold md:rounded-r sm:rounded-md md:rounded-s-0 hover:bg-green-400">
                         Subscribe
                     </button>
                 </form>
