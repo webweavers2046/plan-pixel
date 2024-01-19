683e4bbfddc0253032c2d9e3ed68f60d234a1179
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2024-01-19 16:56:15 +0600

    Hero Section Added

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,13 @@ import OurCompany from "@/pages/Home/Company/OurCompany";
 import GetUpdate from "@/pages/Home/GetUpdate/GetUpdate";
 import HowWorks from "@/pages/Home/howWorks/HowWorks";
 import Testimonial from "@/components/Testimonial";
+import Header from "@/pages/Home/Header/Header";
 
 export default function Home() {
     return (
         <main className="">
-            <div className="bg-[#e2e2e267] py-8">
+            <Header/>
+            <div>
                 <OurCompany />
             </div>
             <KeyFeatures />
