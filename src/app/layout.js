8c901954e61bcedb5489bd892f865fcbd02c07a4
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2024-01-19 09:06:26 +0600

    Seted the projects max width

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={lato.className}>
+      <body className={`${lato.className} mx-auto max-w-screen-xl`}>
         <Navbar/>
         {children}
         <Footer/>
