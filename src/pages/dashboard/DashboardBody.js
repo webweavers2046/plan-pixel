eb7fbc75d2ae62503fdd2bf1957a4b060e3cb30e
sajid360 <ahteshamsajid8@gmail.com>
2024-01-24 01:04:25 +0600

    sidebar and navbar added in the dashboard

---
+++ src/pages/dashboard/DashboardBody.js
@@ -0,0 +1,15 @@
+
+const DashboardBody = () => {
+    return (
+        <div className="mt-10 flex gap-x-8">
+            <div className="w-[380px] h-[400px] bg-white rounded-md py-5 px-6 text-xl font-semibold">
+                Time Truck
+            </div>
+            <div className="w-[380px] h-[430px] bg-white rounded-md py-5 px-6 text-xl font-semibold">
+                My Task
+            </div>
+        </div>
+    );
+};
+
+export default DashboardBody;
