63f98ec34c34a5b2613387eceaca10b96587aabf
Thejellyfish1024 <alamin102410@gmail.com>
2024-01-23 17:24:30 +0600

    added contact informations section

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,11 +1,12 @@
-
+import { FaLinkedinIn, FaFacebookF, FaTwitter } from "react-icons/fa";
 
 const page = () => {
     return (
-        <div className="mt-40">
+        <div className="mt-40 mb-32 lg:px-24">
+            {/* Contact information */}
             <div>
                 <h2 className="text-5xl font-bold mb-16">Contact Us</h2>
-                <ul className="text-xl">
+                <ul className="text-xl mb-8 space-y-4">
                     <li>
                         Location: Dhaka Bangladesh
                     </li>
@@ -16,6 +17,17 @@ const page = () => {
                         Phone: +880 197070 6676
                     </li>
                 </ul>
+                <div className="flex gap-6 ">
+                    <a href="">
+                        <FaLinkedinIn className="text-4xl"></FaLinkedinIn>
+                    </a>
+                    <a href="">
+                        <FaFacebookF className="text-4xl"></FaFacebookF>
+                    </a>
+                    <a href="">
+                        <FaTwitter className="text-4xl"></FaTwitter>
+                    </a>
+                </div>
             </div>
         </div>
     );
