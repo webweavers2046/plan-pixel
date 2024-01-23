9d3d899d4e7f6d29b6331a7b56220a399328be75
Thejellyfish1024 <alamin102410@gmail.com>
2024-01-23 17:07:19 +0600

    created contact page

---
+++ src/app/contact/page.js
@@ -0,0 +1,24 @@
+
+
+const page = () => {
+    return (
+        <div className="mt-40">
+            <div>
+                <h2 className="text-5xl font-bold mb-16">Contact Us</h2>
+                <ul className="text-xl">
+                    <li>
+                        Location: Dhaka Bangladesh
+                    </li>
+                    <li>
+                        Email: webweavers2046@gmail.com
+                    </li>
+                    <li>
+                        Phone: +880 197070 6676
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default page;
