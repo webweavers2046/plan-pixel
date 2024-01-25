c7af15a807fa0ab9768869cf1ab8174e0cd8b6e5
SabbirSami <smd71430@gmail.com>
2024-01-25 13:13:09 +0600

    fix dashboard

---
+++ src/app/(withoutNavbarFooter)/layout.js
@@ -0,0 +1,22 @@
+import DashboardNavbar from "./dashboard/Components/DashboardNavbar";
+import DashboardSidebar from "./dashboard/Components/DashboardSidebar";
+
+export default function DashboardLayout({
+    children, // will be a page or nested layout
+}) {
+    return (
+        <section>
+            <div className="grid grid-cols-12 h-screen">
+                <div className="col-span-2">
+                    <DashboardSidebar />
+                </div>
+                <div className="col-span-10">
+                    <div className="">
+                        <DashboardNavbar />
+                    </div>
+                    {children}
+                </div>
+            </div>
+        </section>
+    );
+}
