c402298e9cd98d7a5222e36c7aa52f7827292f9f
SabbirSami <smd71430@gmail.com>
2024-01-26 11:09:21 +0600

    updated work

--- src/app/(withDashboardNavbarSidebar)/dashboard/tasks/page.js
+++ src/app/(withDashboardNavbarSidebar)/dashboard/tasks/page.js
@@ -1,5 +1,138 @@
+"use client";
+
+import { LuListTodo } from "react-icons/lu";
+import { MdOutlineCallMissedOutgoing } from "react-icons/md";
+import { MdOutlineFileDownloadDone } from "react-icons/md";
+import { FiPlusSquare } from "react-icons/fi";
+
+import Task from "./Task";
+import "@/styles/globals.css";
+import { useEffect, useState } from "react";
+
 const Tasks = () => {
-    return <div>Task</div>;
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try {
+                const response = await fetch("../../../../../public/task.json");
+
+                if (!response.ok) {
+                    throw new Error("Failed to fetch tasks");
+                }
+
+                const data = await response.json();
+                setTasks(data);
+            } catch (error) {
+                console.error("Error fetching tasks:", error);
+            }
+        };
+
+        fetchTasks();
+    }, []);
+
+    console.log(tasks);
+
+    return (
+        <section className="text-white p-6 w-full">
+            {/* header section  */}
+            <div className="md:flex justify-between items-end border-b pb-6 border-white/50">
+                <div className="">
+                    <h6 className="font-medium">TaskTo tasks board</h6>
+                    <p className="opacity-70 font-light text-sm">
+                        Create and complete and manage your tasks using TaskTo
+                        task board.
+                    </p>
+                </div>
+                <div className="">
+                    <button className="bg-white text-black text-sm px-5 py-3 rounded-md font-bold">
+                        <FiPlusSquare className="inline mb-1 me-2 text-xl" />{" "}
+                        Add Task
+                    </button>
+                </div>
+            </div>
+            <div className="grid md:grid-cols-4 lg:gap-6 gap-2  mt-6">
+                <div>
+                    <div className="bg-gray-300/20 text-black px-6 py-4 flex items-center gap-4 rounded-md">
+                        <LuListTodo className="text-2xl" />{" "}
+                        <h2 className="">Upcoming</h2>
+                    </div>
+                    <div className="mt-8">
+                        {tasks.map(
+                            (task, idx) =>
+                                task.status === "to-do" && (
+                                    <Task
+                                        idx={idx}
+                                        key={task._id}
+                                        refetch={refetch}
+                                        task={task}
+                                    />
+                                )
+                        )}
+                    </div>
+                </div>
+                <div>
+                    <div className="bg-gray-300/20 text-black px-6 py-4 flex items-center gap-4 rounded-md">
+                        <LuListTodo className="text-2xl" />{" "}
+                        <h2 className="">To Do</h2>
+                    </div>
+                    <div className="mt-8">
+                        {tasks.map(
+                            (task, idx) =>
+                                task.status === "to-do" && (
+                                    <Task
+                                        idx={idx}
+                                        key={task._id}
+                                        refetch={refetch}
+                                        task={task}
+                                    />
+                                )
+                        )}
+                    </div>
+                </div>
+
+                <div>
+                    <div className="bg-gray-300/20 text-black px-6 py-4 flex items-center gap-4 rounded-md">
+                        <MdOutlineCallMissedOutgoing className="text-2xl" />{" "}
+                        <h2 className="">Working On</h2>
+                    </div>
+                    <div className="mt-8">
+                        {tasks.map(
+                            (task, idx) =>
+                                task.status === "ongoing" && (
+                                    <Task
+                                        idx={idx}
+                                        key={task._id}
+                                        refetch={refetch}
+                                        task={task}
+                                    />
+                                )
+                        )}
+                    </div>
+                </div>
+
+                <div>
+                    <div className="bg-gray-300/20 text-black px-6 py-4 flex items-center gap-4 rounded-md">
+                        <MdOutlineFileDownloadDone className="text-2xl" />{" "}
+                        <h2 className="">Complete</h2>
+                    </div>
+                    <div className="mt-8">
+                        {tasks.map(
+                            (task, idx) =>
+                                task.status === "complete" && (
+                                    <Task
+                                        key={task._id}
+                                        refetch={refetch}
+                                        idx={idx}
+                                        task={task}
+                                    />
+                                )
+                        )}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Tasks;
--- src/app/_app.js
+++ src/app/_app.js
@@ -1,13 +1,21 @@
 import MainLayout from "@/components/layout/MainLayout";
 import "@/styles/globals.css";
+import { Hydrate } from "react-query/hydration";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }) => {
     const getLayout = Component.getLayout || ((page) => page);
 
     return getLayout(
-        <MainLayout>
-            <Component {...pageProps} />
-        </MainLayout>
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <MainLayout>
+                    <Component {...pageProps} />
+                </MainLayout>
+            </Hydrate>
+        </QueryClientProvider>
     );
 };
 
