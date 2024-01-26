3121c7d627dab3cc1567b0c28155528b27fd72eb
Thejellyfish1024 <alamin102410@gmail.com>
2024-01-26 18:38:38 +0600

    solved the conflict

--- src/app/(withDashboardNavbarSidebar)/dashboard/page.js
+++ src/app/(withDashboardNavbarSidebar)/dashboard/page.js
@@ -1,10 +1,29 @@
-import "@/styles/globals.css";
+
+import Calender from "./home/Calender";
+import MyTask from "./home/MyTask";
+import TeamMembers from "./home/TeamMembers";
+import TimeTruck from "./home/TimeTruck";
 
 const Dashboard = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-content-center gap-6 mx-10">
+      {/* colum 1 */}
+      <div>
+        <TimeTruck />
+      </div>
+      {/* colum 2 */}
 
-    return <div>
-       Dashboard
-    </div>;
+      <div>
+        <MyTask />
+      </div>
+      {/* colum 1 */}
+
+      <div>
+        <Calender />
+        <TeamMembers />
+      </div>
+    </div>
+  );
 };
 
 
