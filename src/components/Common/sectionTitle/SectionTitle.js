8716e018ef7379c134b74edd4c29d9503622aecc
SabbirSami <smd71430@gmail.com>
2024-02-01 11:30:47 +0600

    fix get update design

--- src/components/Common/sectionTitle/SectionTitle.js
+++ src/components/Common/sectionTitle/SectionTitle.js
@@ -1,16 +1,16 @@
 const SectionTitle = ({ title, isSetWidth, marginT, marginB }) => {
-  // isSetWidth for control title break down
-  return (
-    <div className="text-center mb-10">
-      <h1
-        className={`font-bold md:text-4xl text-2xl md:leading-[42px]
+    // isSetWidth for control title break down
+    return (
+        <div className="text-center mb-10">
+            <h1
+                className={`lg:text-6xl md:text-5xl text-2xl text-center font-semibold md:mb-16 mb-2 md:leading-[42px]
         ${marginT && `my-${marginT}`}
         ${marginB && `my-${marginB}`}
-         ${isSetWidth ? "max-w-[400px] mx-auto" : ""}`}
-      >
-        {title}
-      </h1>
-    </div>
-  );
+         ${isSetWidth ? "max-w-[600px] mx-auto" : ""}`}
+            >
+                {title}
+            </h1>
+        </div>
+    );
 };
 export default SectionTitle;
