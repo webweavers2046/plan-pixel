72a0a41b13dee9a71ed86fcb73c133caeed153d5
sajid360 <ahteshamsajid8@gmail.com>
2024-02-03 22:48:53 +0600

    do not marge the code

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://plan-pixel-backend-jet.vercel.app/",
+  baseURL: "https://plan-pixel-backend-esp08h4gq-shakil-ahmmed8882.vercel.app",
 });
 
 const apiConnector = () => {
