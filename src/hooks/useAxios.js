ef062e9cd5b720486f904e64ec99280dd7d550b7
Web Weavers <156602277+webweavers2046@users.noreply.github.com>
2024-02-12 07:25:23 +0600

    Update useAxios.js

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    // baseURL: "http://localhost:5000",
     // baseURL: "https://plan-pixel-backend-copy.vercel.app",
+    baseURL: "https://plan-pixel-backend.vercel.app",
 });
 
 const useAxios = () => {
