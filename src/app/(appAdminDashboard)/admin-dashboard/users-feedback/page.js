e48e75b333364e2a412334a0f024a6d4d34bb10b
SabbirSami <smd71430@gmail.com>
2024-02-14 19:27:43 +0600

    fix user feedback

--- src/app/(appAdminDashboard)/admin-dashboard/users-feedback/page.js
+++ src/app/(appAdminDashboard)/admin-dashboard/users-feedback/page.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const UserFeedback = () => {
     const totalNumberOfFeedback = 18;
     const feedbackData = [
@@ -122,6 +120,61 @@ const UserFeedback = () => {
                 "I've been using the task management site primarily on my mobile device, and while it's functional, there's room for improvement in the mobile experience. Navigating through the site on a smaller screen can be a bit cumbersome at times, especially when dealing with multiple tasks.",
             userImage: "https://example.com/sarah-martinez.jpg",
         },
+        {
+            userName: "Emily Brown",
+            userEmail: "emily@example.com",
+            date: "2024-02-12",
+            feedback:
+                "Additionally, the site's performance has been exceptional; it's fast, reliable, and doesn't lag even when handling large volumes of data. Kudos to the development team for creating such a fantastic tool!",
+            userImage: "https://example.com/emily-brown.jpg",
+        },
+        {
+            userName: "Sarah Martinez",
+            userEmail: "sarah@example.com",
+            date: "2024-02-10",
+            feedback:
+                "I've been using the task management site primarily on my mobile device, and while it's functional, there's room for improvement in the mobile experience. Navigating through the site on a smaller screen can be a bit cumbersome at times, especially when dealing with multiple tasks. ",
+            userImage: "https://example.com/sarah-martinez.jpg",
+        },
+        {
+            userName: "Alice Johnson",
+            userEmail: "alice@example.com",
+            date: "2024-02-14",
+            feedback:
+                "The features are robust, and it helps me stay organized like never before. I particularly appreciate the ability to set deadlines, prioritize tasks, and collaborate with my team seamlessly. Overall, I'm extremely satisfied with the site, and I highly recommend it to anyone looking to boost their productivity.",
+            userImage: "https://example.com/alice-johnson.jpg",
+        },
+        {
+            userName: "Bob Smith",
+            userEmail: "bob@example.com",
+            date: "2024-02-13",
+            feedback: "I plan to continue using it in the future.",
+            userImage: "https://example.com/bob-smith.jpg",
+        },
+        {
+            userName: "Emily Brown",
+            userEmail: "emily@example.com",
+            date: "2024-02-12",
+            feedback:
+                "Additionally, the site's performance has been exceptional; it's fast, reliable, and doesn't lag even when handling large volumes of data. Kudos to the development team for creating such a fantastic tool!",
+            userImage: "https://example.com/emily-brown.jpg",
+        },
+        {
+            userName: "Michael Wilson",
+            userEmail: "michael@example.com",
+            date: "2024-02-11",
+            feedback:
+                "I've had with the site. I look forward to seeing how it evolves in the future.",
+            userImage: "https://example.com/michael-wilson.jpg",
+        },
+        {
+            userName: "Sarah Martinez",
+            userEmail: "sarah@example.com",
+            date: "2024-02-10",
+            feedback:
+                "I've been using the task management site primarily on my mobile device, and while it's functional, there's room for improvement in the mobile experience. Navigating through the site on a smaller screen can be a bit cumbersome at times, especially when dealing with multiple tasks.",
+            userImage: "https://example.com/sarah-martinez.jpg",
+        },
     ];
 
     const totalCardInRow = Math.ceil(totalNumberOfFeedback / 4);
@@ -129,6 +182,8 @@ const UserFeedback = () => {
     const row2 = row1 + row1;
     const row3 = row2 + row1;
     const row4 = row3 + row1;
+
+    console.log(totalCardInRow);
 
     return (
         <section>
@@ -174,7 +229,7 @@ const UserFeedback = () => {
                             </div>
                         ))}
                     </div>
-                    <div className="grid md:hidden xl:grid gap-6">
+                    <div className="grid gap-6">
                         {feedbackData.slice(row3, row4).map((feedback, idx) => (
                             <div
                                 className="bg-dashboardPrimaryColor h-full bg-gradient-to-r hover:from-[#94f3b0] hover:to-[#7abf88] hover:text-black p-10 rounded-lg
