6b8d018041d1be3d1c1c8a2823c6e632c25df8b6
Web Weavers <156602277+webweavers2046@users.noreply.github.com>
2024-02-15 14:56:55 +0600

    Update useAxios.js

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
   // baseURL: "https://plan-pixel-backend-jet.vercel.app/",
-  // baseURL: "https://plan-pixel-backend.vercel.app/",
+  baseURL: "https://plan-pixel-backend.vercel.app/",
 });
 
 const useAxios = () => {
