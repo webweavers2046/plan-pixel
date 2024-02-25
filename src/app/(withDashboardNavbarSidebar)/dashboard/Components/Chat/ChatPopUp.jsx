4b767c0152aa9ad50b72f00a226e5cacd16fbd49
Forhad96 <forhadairdrop@gmail.com>
2024-02-25 14:49:25 +0600

    chat popup ui improve

--- src/app/(withDashboardNavbarSidebar)/dashboard/Components/Chat/ChatPopUp.jsx
+++ src/app/(withDashboardNavbarSidebar)/dashboard/Components/Chat/ChatPopUp.jsx
@@ -6,6 +6,8 @@ import useGlobalContext from "@/hooks/useGlobalContext";
 import { useRouter } from "next/navigation";
 import useAxios from "@/hooks/useAxios";
 import useTranstackData from "@/hooks/useTanstack/useTranstackData";
+import { FaSearch } from "react-icons/fa";
+import { FaAlignLeft, FaCircleCheck } from "react-icons/fa6";
 const socket = socketIO.connect("http://localhost:5000");
 // import "./chat.css";
 
@@ -36,7 +38,7 @@ const ChatPopUp = ({ isOpenPopUp, setIsOpenPopUp }) => {
     const messageListener = (data) => {
       setMessages((prevMessages) => [...prevMessages, data]);
       // console.table(data);
-      saveMessage(data)
+      saveMessage(data);
     };
 
     socket.on("messageResponse", messageListener);
@@ -51,7 +53,9 @@ const ChatPopUp = ({ isOpenPopUp, setIsOpenPopUp }) => {
       setUsers(data);
     });
   }, []);
-
+  useEffect(() => {
+    socket.on("typingResponse", (data) => setTypingStatus(data));
+  }, [socket]);
 
   useEffect(() => {
     if (data) {
@@ -60,29 +64,42 @@ const ChatPopUp = ({ isOpenPopUp, setIsOpenPopUp }) => {
     }
   }, [data]);
 
-async function saveMessage(data) {
-  try {
-    if (data) {
-      const res = await xios.post(`/message`, data);
-      console.log(res);
+  useEffect(() => {
+    // 👇️ scroll to bottom every time messages change
+    lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  async function saveMessage(data) {
+    try {
+      if (data) {
+        const res = await xios.post(`/message`, data);
+        console.log(res);
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
   }
-}
+  console.log(activeWorkspace);
   return (
     <div
-      className={`h-[85vh] rounded-xl z-50 shadow-2xl bg-green-100 fixed top-20 ${
+      className={`h-[85vh] rounded-lg z-50 shadow-2xl bg-[#2C293D] fixed top-20 ${
         isOpenPopUp ? " ml-5 right-1 " : "-right-full"
       }   w-[400px] flex flex-col`}
     >
+      <div className="bg-[#35C179] rounded-tr-lg rounded-tl-lg text-white h-16 p-4 flex justify-between">
+        <div className="flex gap-4 items-center ">
+          <FaAlignLeft className="h-6 w-6" />
+          <h1 className="text-xl">{activeWorkspace.title}</h1>
+        </div>
+        <FaSearch className="h-6 w-6" />
+      </div>
       <div className=" flex-1 overflow-y-scroll">
         {messages?.map((message) => (
           <div className="px-4 py-2">
             {message?.name !== user?.email && (
               <>
-                <p className="font-medium italic text-xs mb-1">
-                  {message.name}
+                <p className="font-medium italic text-gray-200 text-xs mb-1">
+                  {typingStatus ? typingStatus : message.name}
                 </p>
                 <div className="flex items-center mb-2">
                   <img
@@ -90,7 +107,7 @@ async function saveMessage(data) {
                     src="https://picsum.photos/50/50"
                     alt="User Avatar"
                   />
-                  <div className="bg-red-200 rounded-lg p-2 shadow mb-2  px-4 py-2 max-w-[80%]">
+                  <div className="bg-[#36C179] min-w-32 text-white text-lg rounded-tl-none rounded-md p-2 shadow mb-2  px-4 py-2 max-w-[80%]">
                     {message?.name !== user?.email && message?.text}
                   </div>
                 </div>
@@ -99,18 +116,19 @@ async function saveMessage(data) {
 
             {message?.name === user?.email && (
               <>
-                <p className="font-medium italic text-xs mb-1  text-right">
+                <p className="font-medium italic text-xs mb-1 text-gray-200 text-right">
                   You
                 </p>
                 <div className="flex items-center justify-end">
-                  <div className="bg-[#50B577] text-white rounded-lg p-2 shadow mr-2  px-4 py-2 max-w-[80%]">
+                  <div className="bg-[#36C179] min-w-32 text-white text-lg rounded-md rounded-tr-none p-2 shadow mr-2  px-4 py-2 max-w-[80%]">
                     {message?.name === user?.email && message?.text}
                   </div>
-                  <img
+                  {/* <img
                     className="w-8 h-8 rounded-full"
                     src="https://picsum.photos/50/50"
                     alt="User Avatar"
-                  />
+                  /> */}
+                  <FaCircleCheck className="text-white w-6 h-6" />
                 </div>
               </>
             )}
@@ -119,11 +137,13 @@ async function saveMessage(data) {
 
         <div ref={lastMessageRef} />
       </div>
+
+      <p className="text-sm italic ml-6">{typingStatus}</p>
       <ChatPopFooter />
-      <p>user count: {users?.length}</p>
+      {/* <p>user count: {users?.length}</p>
       {users?.map((user) => (
         <p>{user.userName}</p>
-      ))}
+      ))} */}
     </div>
   );
 };
@@ -136,11 +156,28 @@ const ChatPopFooter = () => {
   const [message, setMessage] = useState("");
   const [userName, setUserName] = useState("");
   const { activeWorkspace } = useGlobalContext();
+  const [typingTimeout, setTypingTimeout] = useState(null);
   useEffect(() => {
     if (user) {
       setUserName(user?.email);
     }
   }, [user]);
+const handleTyping = () => {
+
+    // Clear any previous typing timeout
+    clearTimeout(typingTimeout);
+
+    // Emit typing event
+    socket.emit("typing", `${userName} is typing..`);
+
+    // Set a new typing timeout to clear typing status after 2 seconds (adjust as needed)
+    setTypingTimeout(
+      setTimeout(() => {
+        socket.emit("typing", ""); // Clear typing status
+      }, 2000)
+    ); // 2 seconds
+
+};
 
   const handleSendMessage = (e) => {
     e.preventDefault();
@@ -165,7 +202,7 @@ const ChatPopFooter = () => {
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           // {/*OnKeyDown function*/}
-          // onKeyDown={handleTyping}
+          onKeyDown={handleTyping}
         />
         <button
           type="submit"
@@ -177,5 +214,3 @@ const ChatPopFooter = () => {
     </div>
   );
 };
-
-
