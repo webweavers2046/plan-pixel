dc80aaac13accf25ae01effd8db5bea76830a57e
Thejellyfish1024 <alamin102410@gmail.com>
2024-03-01 11:23:22 +0600

    modified the code of tasks page

--- src/app/(withDashboardNavbarSidebar)/dashboard/tasks/page.js
+++ src/app/(withDashboardNavbarSidebar)/dashboard/tasks/page.js
@@ -59,7 +59,7 @@ const Tasks = () => {
 
   const workspaceAllTasks =
     activeWorkspaceTasks.length > 0 ? activeWorkspaceTasks : allWorkspaceTasks;
-    
+
     console.log('all tasks of workspace',workspaceAllTasks);
 
   // Tasks in different status
@@ -165,9 +165,9 @@ const Tasks = () => {
               </button>
 
 
-              <div>
+              {/* <div>
                 <Toggler enabled={isTogglerEnabled} setEnabled={setIsTogglerEnabled}/>
-              </div>
+              </div> */}
             </div>
           </div>
 
@@ -362,12 +362,12 @@ const Tasks = () => {
           ></TaskModal>
 
           <CardDetailsModal></CardDetailsModal>
-          {
+          {/* {
                 isTogglerEnabled &&
                 <button onClick={ isActive === "all-tasks"? handleMultipleArchive:handleMultipleUnArchive} className="bg-rose-600 fixed bottom-8 right-2 z-50 text-white p-2 rounded-lg">{
                   isActive === "all-tasks"?"Archive multiple":"Unarchive multiple"
                 } </button>
-          }
+          } */}
         </section>
       )}
     </>
